75b1cda6909bd63bf0b4b67dd3c1fab47f95133b
JoseRodriguez <joselrc295@gmail.com>
2024-08-06 15:02:47 -0500

    inicio

---
+++ tatiana-website/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        {/* Agrega más rutas aquí */}
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
