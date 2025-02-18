e575adfe175638d9d69cd3700e438b6a92e7d87a
Jose Luis Rodriguez Cano <165174282+JoseRo295@users.noreply.github.com>
2025-02-17 23:21:55 -0500

    mejora corrusel

--- tg-design/src/components/Corrusel.js
+++ tg-design/src/components/Corrusel.js
@@ -1,85 +1,63 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import 'swiper/css/autoplay';
-import '../Styles/style.css' // Asegúrate de que todos los estilos necesarios están importados
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+import "../Styles/style.css"; // Asegúrate de que todos los estilos necesarios están importados
 
 // Import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
-// import image1 from "../Images/corrusel/1.png";
-// import image2 from "../Images/corrusel/2.png";
-// import image3 from "../Images/corrusel/3.png";
-// import image4 from "../Images/corrusel/4.png";
-// import image5 from "../Images/corrusel/Безимени-1.png";
-
-// import image1 from "../Images/corrusel2/1.png";
-import image2 from "../Images/corrusel2/2.png";
-import image3 from "../Images/corrusel2/3.png";
-import image4 from "../Images/corrusel2/4.png";
-import image5 from "../Images/corrusel2/5.png";
-import image6 from "../Images/corrusel2/6.png";
-
+// Importa las imágenes del carrusel
 import image1 from "../Images/corrusel/5.png";
-
-
+import image2 from "../Images/corrusel/6.jpg";
 
 const ImageCarousel = () => {
   return (
-    <Swiper
-      modules={[Autoplay, Pagination, Navigation]} // Asegúrate de registrar los módulos que vas a usar
-      spaceBetween={30}
-      centeredSlides={true}
-      slidesPerView={1}
-      autoplay={{
-        delay: 5000,
-        disableOnInteraction: false
-      }}
-      pagination={{
-        clickable: true
-      }}
-      navigation={true}
-      loop={true}
-      className="mySwiper" // Puedes definir estilos adicionales en styles.css si es necesario
-    >
-      {[
-        image1,
-        image2,
-        image3,
-        image4,
-        image5,
-        image6,
-        
-      ].map((image, index) => (
-        <SwiperSlide key={index}>
-  <div
-    style={{
-      height: "85vh", // Mantiene la altura del carrusel
-      width: "100%", // Asegura que ocupe todo el ancho
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-    }}
-  >
-    <img
-      src={image}
-      alt={`Slide ${index + 1}`}
-      style={{
-        width: "100%", // Se ajusta al contenedor
-        height: "100%", // Ocupa el 100% del contenedor
-        objectFit: "contain", // Evita que se recorte la imagen
-        borderRadius: "10px", // Agrega bordes redondeados si es necesario
-      }}
-    />
-  </div>
-</SwiperSlide>
+    <div className="relative w-full h-screen">
+      {/* Swiper Carrusel */}
+      <Swiper
+        modules={[Autoplay, Pagination, Navigation]}
+        spaceBetween={0}
+        centeredSlides={true}
+        slidesPerView={1}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        loop={true}
+        className="absolute top-0 left-0 w-full h-full"
+      >
+        {[image1, image2].map((image, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="w-full h-screen bg-cover bg-center"
+              style={{ backgroundImage: `url(${image})` }}
+            >
+              {/* Superposición Oscura */}
+              <div className="absolute inset-0 bg-black bg-opacity-40"></div>
 
-      ))}
-    </Swiper>
+              {/* Contenido Centrado */}
+              <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white px-6">
+                <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-4">
+                  Get a Designer Space You'll Love
+                </h1>
+                <button className="bg-[#A8957A] hover:bg-[#8C7A5F] text-white font-semibold py-3 px-6 rounded-lg text-lg transition duration-300">
+                  Start My Transformation
+                </button>
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
